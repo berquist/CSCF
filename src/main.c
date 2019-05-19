9c0298f9b9657fddd79daccb98262f8bf20ed299
abdullah <gxb14174@uni.strath.ac.uk>
2019-05-19 16:16:44 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Very simple HF implementation for H2. 
+ * Parser for files etc. will be added later.
+ * Right now, parameters and geometry is hard-coded.
+ * This is so I can focus on the maths. 
+ * Once I've got it working for one model, I'll start working on testing as well.
+ */
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
