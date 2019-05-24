92f1bf88c2c0a05430fbcc0d53168232fe88a42a
Eric Berquist <eric.berquist@gmail.com>
2019-05-23 21:57:59 -0400

    Add more overlap-related tests

--- tests/test_overlap.c
+++ tests/test_overlap.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include "THO.h"
+#include "utils.h"
 
 int compare(double computed, double ref, double thresh) {
     const double diff = fabs(computed - ref);
@@ -10,19 +11,69 @@ int compare(double computed, double ref, double thresh) {
     return retval;
 }
 
+/* int test_dist2() { */
+/*     return 0; */
+/* } */
+
+int test_double_factorial() {
+    printf("double_factorial\n");
+    if (double_factorial(0) != 1) {
+        printf("failure in double_factorial(0)\n");
+        return 1;
+    }
+    if (double_factorial(1) != 1) {
+        printf("failure in double_factorial(1)\n");
+        return 1;
+    }
+    if (double_factorial(2) != 2) {
+        printf("failure in double_factorial(2)\n");
+        return 1;
+    }
+    if (double_factorial(3) != (3 * 1)) {
+        printf("failure in double_factorial(3)\n");
+        return 1;
+    }
+    if (double_factorial(4) != (4 * 2)) {
+        printf("failure in double_factorial(4)\n");
+        return 1;
+    }
+    if (double_factorial(5) != (5 * 3 * 1)) {
+        printf("failure in double_factorial(5)\n");
+        return 1;
+    }
+    return 0;
+}
+
+int test_f() {
+    printf("f\n");
+    double thresh = 1.0e-7;
+
+    if (compare(f(1, 1, 1, 0.1, 0.2), 0.3, thresh))
+        return 1;
+    if (compare(f(1, 1, 1, 0.3, 0.4), 0.7, thresh))
+        return 1;
+    if (compare(f(1, 3, 1, 0.1, 0.2), 0.007, thresh))
+        return 1;
+    if (compare(f(2, 3, 1, 0.1, 0.2), 0.09, thresh))
+        return 1;
+
+    return 0;
+}
+
 int test_overlap1d() {
-    int l1 = 1;
-    int l2 = 1;
-    double PAx = 0.1;
-    double PBx = 0.2;
-    double gamma = 1.0;
-    double computed = overlap_1d(l1, l2, PAx, PBx, gamma);
-    double ref = 0.52;
+    printf("overlap1d\n");
     double thresh = 1.0e-5;
-    return compare(computed, ref, thresh);
+
+    if (compare(overlap_1d(1, 1, 0.1, 0.2, 1.0), 0.52, thresh))
+        return 1;
+    if (compare(overlap_1d(3, 1, 0.1, 0.2, 1.0), 0.7952, thresh))
+        return 1;
+
+    return 0;
 }
 
 int test_full_overlap_1() {
+    printf("full_overlap_1\n");
     int lmn1[3] = {0, 0, 0};
     double A[3] = {0.0, 0.0, 0.0};
     double a = 1.8;
@@ -36,6 +87,7 @@ int test_full_overlap_1() {
 }
 
 int test_full_overlap_2() {
+    printf("full_overlap_2\n");
     int lmn1[3] = {1, 0, 0};
     double A[3] = {0.0, 0.0, 0.0};
     double a = 1.8;
@@ -49,7 +101,9 @@ int test_full_overlap_2() {
 }
 
 int main() {
-    return test_overlap1d() |
+    return test_double_factorial() |
+        test_f() |
+        test_overlap1d() |
         test_full_overlap_1() |
         test_full_overlap_2() |
         0;
